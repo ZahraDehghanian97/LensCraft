ed923bab9d543eb6a85f0ae42e1b2af8f967ace6
Morteza Abolghasemi <a.re.morteza@gmail.com>
2024-12-09 07:52:24 +0330

    add resume_checkpoint

--- src/train.py
+++ src/train.py
@@ -1,6 +1,7 @@
+import os
 import hydra
 from hydra.core.global_hydra import GlobalHydra
-from hydra.utils import instantiate
+from hydra.utils import instantiate, get_class
 from omegaconf import DictConfig, OmegaConf
 import lightning as L
 from data.datamodule import CameraTrajectoryDataModule
@@ -26,8 +27,40 @@ def main(cfg: DictConfig):
 
     model = instantiate(cfg.training.model)
 
-    lightning_model = instantiate(
-        cfg.training, model=model, compile_mode=cfg.compile.mode, compile_enabled=cfg.compile.enabled, dataset_mode=data_module.dataset_mode)
+    optimizer = instantiate(cfg.training.optimizer)
+    lr_scheduler = instantiate(cfg.training.lr_scheduler)
+
+    LightningModuleClass = get_class(cfg.training._target_)
+
+    if hasattr(cfg, 'resume_checkpoint') and cfg.resume_checkpoint:
+        checkpoint_path = cfg.resume_checkpoint
+        if not os.path.exists(checkpoint_path):
+            raise FileNotFoundError(f"Checkpoint not found at: {checkpoint_path}")
+        print(f"Loading model from checkpoint: {checkpoint_path}")
+        
+        lightning_model = LightningModuleClass.load_from_checkpoint(
+            checkpoint_path,
+            model=model,
+            optimizer=optimizer,
+            lr_scheduler=lr_scheduler,
+            noise=cfg.training.noise,
+            mask=cfg.training.mask,
+            teacher_forcing_schedule=cfg.training.teacher_forcing_schedule,
+            compile_mode=cfg.compile.mode,
+            compile_enabled=cfg.compile.enabled,
+            dataset_mode=data_module.dataset_mode,
+        )
+        print("Checkpoint loaded successfully")
+    else:
+        lightning_model = instantiate(
+            cfg.training,
+            model=model,
+            optimizer=optimizer,
+            lr_scheduler=lr_scheduler,
+            compile_mode=cfg.compile.mode,
+            compile_enabled=cfg.compile.enabled,
+            dataset_mode=data_module.dataset_mode
+        )
 
     callbacks = [instantiate(cb_conf) for cb_conf in cfg.callbacks.values()]
 
