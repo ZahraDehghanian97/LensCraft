55396f0a7c06e34e0ab4c8e2d3f5a50bb3e0fc72
AmirVahedi18 <amirvahedi81328@gmail.com>
2025-04-23 21:30:25 +0330

    prevent loss of None embeddings to backpropagate

--- src/training/losses/losses.py
+++ src/training/losses/losses.py
@@ -4,7 +4,6 @@ from .angle_loss import AngleLoss
 import random
 import numpy as np
 from data.simulation.constants import CLIP_PARAMETERS_DICT
-import time
 
 
 class CameraTrajectoryLoss:
@@ -88,7 +87,8 @@ class CameraTrajectoryLoss:
                 weighted_clip_loss=self.weighted_clip_loss, 
                 clip_weights=self.clip_weights, 
                 sum_clip_weights=self.sum_clip_weights,
-                encoder_loss_function=self.encoder_loss_function)
+                encoder_loss_function=self.encoder_loss_function,
+                batch=batch)
             if self.weighted_clip_loss:
                 total_loss += total_clip_loss_weighted / clip_pred.shape[1] * self.clip_loss_scaling_factor
             else:
@@ -209,7 +209,6 @@ class CameraTrajectoryLoss:
 
             for _ in range(N_SIMILAR_SAMPLES): 
                 n_modification = random.randint(1, 4)
-                start = time.perf_counter()
                 clip_sample_similar = self.modify_sample(clip_embedding_parameters, 
                                                          clip_sample_target, 
                                                          n_modification).to(self.device)
@@ -261,23 +260,25 @@ class CameraTrajectoryLoss:
         return torch.tensor(contrastive_loss).clone().detach()
 
 
-
     
-    @staticmethod
-    def compute_clip_loss(clip_target, 
+    def compute_clip_loss(self,
+                          clip_target, 
                           clip_pred, 
                           n_clip_embs, 
                           weighted_clip_loss, 
                           clip_weights, 
                           sum_clip_weights, 
-                          encoder_loss_function):
+                          encoder_loss_function, 
+                          batch):   
+        none_mask = self.create_none_mask_matrix(batch, n_clip_embs)
         clip_losses = []
         total_clip_loss = 0
         total_clip_loss_weighted = 0
         for i in range(n_clip_embs):
+            embedding_none_mask = none_mask[:, i]
             if encoder_loss_function == "clip":
                 similarity = cosine_similarity(clip_target[i], clip_pred[i])
-                current_loss = 1 - similarity.mean()
+                current_loss = 1 - similarity[embedding_none_mask].mean()
             elif encoder_loss_function == "mse":
                 current_loss = mse_loss(clip_target[i], clip_pred[i])
             clip_losses.append(current_loss) 
@@ -288,6 +289,21 @@ class CameraTrajectoryLoss:
             total_clip_loss_weighted = total_clip_loss_weighted / sum_clip_weights
         total_clip_loss = total_clip_loss / n_clip_embs
         return total_clip_loss_weighted, clip_losses, total_clip_loss
+    
+
+
+    @staticmethod
+    def create_none_mask_matrix(batch: dict, n_clip_embs: int):
+        batch_size = len(batch["camera_trajectory"])
+        none_entries = torch.ones(batch_size, n_clip_embs, dtype=torch.bool)
+        for sample_idx in range(batch_size):
+            clip_embedding_parameters = batch["cinematography_prompt_parameters"][sample_idx] +\
+                                        batch["simulation_instruction_parameters"][sample_idx]
+            for emb_idx in range(n_clip_embs):
+                _, _, value_idx, _ = clip_embedding_parameters[emb_idx]
+                if value_idx == -1:
+                    none_entries[sample_idx, emb_idx] = False
+        return none_entries
     
 
 
