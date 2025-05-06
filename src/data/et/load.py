fe07d7ad568d4806b507282aff68186b178b034b
Morteza Abolghasemi <a.re.morteza@gmail.com>
2025-05-07 00:33:54 +0330

    fix load et config and dataset

--- src/data/et/load.py
+++ src/data/et/load.py
@@ -7,7 +7,7 @@ from torch.utils.data import Dataset
 from utils.importing import ModuleImporter
 
 
-def load_et_config(project_config_dir: str, dataset_dir: str = None, set_name: str = None):
+def load_et_config(project_config_dir: str, config_name: str="config.yaml" , dataset_dir: str = None, set_name: str = None):
     config_rel_path = os.path.dirname(
         os.path.relpath(project_config_dir, os.path.dirname(__file__)))
     
@@ -18,11 +18,11 @@ def load_et_config(project_config_dir: str, dataset_dir: str = None, set_name: s
         overrides.append(f"data_dir={dataset_dir}")
     
     with initialize(version_base=None, config_path=config_rel_path):
-        return compose(config_name="config.yaml", overrides=overrides)
+        return compose(config_name=config_name, overrides=overrides)
 
 
 def load_et_dataset(project_config_dir: str, dataset_dir: str, set_name: str, split: str) -> Dataset:
-    director_cfg = load_et_config(project_config_dir, dataset_dir, set_name)
+    director_cfg = load_et_config(project_config_dir, "config.yaml", dataset_dir, set_name)
 
     with ModuleImporter.temporary_module(os.path.dirname(os.path.dirname(project_config_dir)), ['utils.file_utils', 'utils.rotation_utils']):
         return instantiate(director_cfg.dataset).set_split(split)
