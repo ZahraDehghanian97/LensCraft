fix loss calculation

src/data/convertor.py
@@ -18,7 +18,6 @@ def subject_et_to_sim(subject_trajectory: torch.tensor,
 
     subject_trajectory = torch.tensor(subject_trajectory)
     subject_trajectory_resized = fix_subject_traj_length(subject_trajectory, target_frames=30)
-    # print("+", subject_trajectory_resized.shape)
 
     subject_volume = torch.tensor([[0.5, 1.7, 0.3]])  # Default size values
 
@@ -29,17 +28,14 @@ def subject_et_to_sim(subject_trajectory: torch.tensor,
 def camera_et_to_sim(camera_trajectory: torch.tensor, 
                      seq_len: int):
     is_batch = True
-    # print("1", camera_trajectory.shape)
 
     if len(camera_trajectory.shape) == 3:
         camera_trajectory = camera_trajectory.unsqueeze(0)
         is_batch = False
 
     camera_trajectory_resized = fix_camera_traj_length(camera_trajectory, seq_len)
-    # print("2", camera_trajectory_resized.shape)
     camera_trajectory_resized = et_to_6dof(camera_trajectory_resized)
 
-    # print("3", camera_trajectory_resized.shape)
     
     if not is_batch:
         camera_trajectory_resized = camera_trajectory_resized.squeeze()
@@ -55,8 +51,8 @@ def camera_et_to_sim(camera_trajectory: torch.tensor,
 
 
 
-def subject_ccdm_to_sim(subject_trajectory: None,
-                      seq_len: int):
+def subject_ccdm_to_sim(subject_trajectory = None,
+                        seq_len: int = 30):
     subject_loc_rot = torch.zeros((seq_len, 6), dtype=torch.float32)
     subject_volume = torch.tensor([[0.5, 1.7, 0.3]], dtype=torch.float32)
     

src/training/losses/angle_loss.py
@@ -27,7 +27,7 @@ class AngleLoss(torch.nn.Module):
         pred_normal = self.euler_to_normal(pred)
         target_normal = self.euler_to_normal(target)
         product = cosine_similarity(pred_normal, target_normal)
-        loss = torch.mean(torch.tan(torch.pi / 4 * (1 - product)))
+        loss = torch.mean(torch.tan(torch.pi / 4.1 * (1 - product)))
         # loss = torch.mean(1 / (self.scaling_factor + product) - 1 / (self.scaling_factor + 1))
         return loss
 

src/training/losses/contrastive_loss.py
@@ -111,17 +111,24 @@ class ContrastiveLoss:
                                         self.clip_embeddings[value_type][embedding_key].unsqueeze(0).to(self.device)).mean()
                         contrastive_loss += similarity + 1
                         
-        return torch.tensor(contrastive_loss).clone().detach()
+        return torch.tensor(contrastive_loss)
+
+
+
 
     def compute_v3(self, clip_pred, clip_target, batch):
         contrastive_loss = 0
-        parameters_size = clip_pred.shape[0]
+        top_1 = 0
         batch_size = clip_pred.shape[1]
         
         for sample_idx in range(batch_size):
             clip_sample_pred = clip_pred[:, sample_idx, :]
             clip_embedding_parameters = batch["cinematography_prompt_parameters"][sample_idx] + \
                                     batch["simulation_instruction_parameters"][sample_idx]
+            
+            counter = 0
+            local_top_1 = 0
+            local_contrastive_loss = 0
             
             for emb_idx, (prefix, data_value, value_idx, _) in enumerate(clip_embedding_parameters):
                 if value_idx == -1:
@@ -134,12 +141,31 @@ class ContrastiveLoss:
                     value_type = "boolean"
                 else:
                     value_type = CLIP_PARAMETERS_DICT[prefix].__name__
-                    
+
+                similarites = []
+                for embedding_key in self.clip_embeddings[value_type].keys():
+                        similarity = cosine_similarity(clip_sample_pred[emb_idx].unsqueeze(0).to(self.device),
+                                                        self.clip_embeddings[value_type][embedding_key].unsqueeze(0).to(self.device))
+                        similarites.append(similarity.item())
+                similarites  = np.array(similarites)  
+                 
+                if similarites.argmin() == value_idx:
+                    local_top_1 += 1
+
                 mean_embedding = self.embedding_means[value_type].to(self.device)
                 similarity = cosine_similarity(
                     clip_sample_pred[emb_idx].unsqueeze(0), 
                     mean_embedding.unsqueeze(0)
                 ).mean()
-                contrastive_loss += similarity + 1
+
+                local_contrastive_loss += similarity + 1
+                counter += 1
+            
+            contrastive_loss += local_contrastive_loss / counter
+            top_1 += local_top_1 / counter
+        
+        print("TOP_1", top_1 / batch_size)
                 
-        return torch.tensor(contrastive_loss / (batch_size * parameters_size)).clone().detach()
+        return contrastive_loss / batch_size
+
+

src/training/losses/losses.py
@@ -72,7 +72,7 @@ class CameraTrajectoryLoss:
 
 
         if self.losses_list.get("clip", 0):
-            total_clip_loss_weighted, clip_losses, total_clip_loss = self.clip_loss.compute(
+            clip_losses, total_clip_loss = self.clip_loss.compute(
                 clip_target=clip_target,
                 clip_pred=clip_pred,
                 n_clip_embs=self.n_clip_embs,
@@ -80,19 +80,15 @@ class CameraTrajectoryLoss:
                 prompt_none_mask=batch.get("prompt_none_mask", None),
                 encoder_loss_function=self.encoder_loss_function
             )
-            
-            if self.weighted_clip_loss:
-                loss_dict["clip"] = total_clip_loss_weighted / clip_pred.shape[1]
-            else:
-                loss_dict["clip"] = total_clip_loss / clip_pred.shape[1]
+
+            loss_dict["clip"] = total_clip_loss
                 
             loss_dict["clip_elements"] = {i: clip_losses[i] for i in range(self.n_clip_embs)} # FIXME
-            loss_dict["average_clip"] = total_clip_loss * 200 # FIXME
 
 
 
         if self.losses_list.get("cycle", 0) and cycle_embeddings is not None:
-            total_cycle_loss_weighted, cycle_losses, total_cycle_loss = self.clip_loss.compute(
+            cycle_losses, total_cycle_loss = self.clip_loss.compute(
                 clip_target=clip_pred,
                 clip_pred=cycle_embeddings,
                 n_clip_embs=self.n_clip_embs,
@@ -100,16 +96,12 @@ class CameraTrajectoryLoss:
                 encoder_loss_function=self.encoder_loss_function
             )
             
-            if self.weighted_clip_loss:
-                loss_dict["cycle"] = total_cycle_loss_weighted / cycle_embeddings.shape[1]
-            else:
-                loss_dict["cycle"] = total_cycle_loss / cycle_embeddings.shape[1]
+            loss_dict["cycle"] = total_cycle_loss 
             
             loss_dict["cycle_elements"] = {i: cycle_losses[i] for i in range(self.n_clip_embs)} # FIXME
-            loss_dict["average_cycle"] = total_cycle_loss * 200 # FIXME
 
         elif self.losses_list.get("cycle", 0) and cycle_embeddings is None:
-            loss_dict["cycle"] = 0
+            loss_dict["cycle"] = torch.tensor(0)
 
 
 

src/visualization.py
@@ -30,7 +30,7 @@ def generate_metric_tags(cfg: DictConfig) -> List[str]:
                 prefixes.append("train")
             for prefix in prefixes:
                 for i in range(cfg.metrics.clips.num_clips):
-                    clip_tags.append(f"{prefix}_clip_{i}_epoch")
+                    clip_tags.append(f"{prefix}_clip_elements_{i}_epoch")
             tags["clip_tags"] = clip_tags
             if cfg.metrics.clips.categorize_embeddings:
                 cinematography_structure = {"cinematography_" + k: v for k, v in cfg.clip_structure.cinematography.items()}
@@ -149,12 +149,12 @@ def get_average_embedding_loss(
         clip_losses: dict[str, dict[str, list]],
         plot_list: list[str],
         n_clip_embeddings: int) -> dict[str, list]:
-    steps = clip_losses["val_clip_0_epoch"]["steps"]
+    steps = clip_losses["val_clip_elements_0_epoch"]["steps"]
     train_values = np.zeros(len(steps))
     valid_values = np.zeros(len(steps))
     for item, value in zip(["train", "val"], [train_values, valid_values]):
         for i in range(n_clip_embeddings):
-            value += np.array(clip_losses[f"{item}_clip_{i}_epoch"]["values"])
+            value += np.array(clip_losses[f"{item}_clip_elements_{i}_epoch"]["values"])
     average_embedding_loss_train = {"steps": steps, "values": list(train_values / n_clip_embeddings)}
     average_embedding_loss_valid = {"steps": steps, "values": list(valid_values / n_clip_embeddings)}
     average_embedding_loss = {
@@ -170,6 +170,7 @@ def plot_scalars(
     cfg: DictConfig,
     save_path: str = None
 ):
+    print(scalar_data.keys())
     initialize(cfg)
     model_version = cfg.logdir.split("/")[-1]
     save_path = save_path + model_version + "." + cfg.output.plot_format
@@ -214,7 +215,8 @@ def plot_scalars(
         ax[row, col].grid(**cfg.plot.style.major_grid_style)
         ax[row, col].grid(**cfg.plot.style.minor_grid_style)
         ax[row, col].minorticks_on()
-        steps = list(list(scalar_data.values())[0].values())[0]["steps"]
+        print(scalar_data.values())
+        steps = list(list(scalar_data.values())[9].values())[0]["steps"]
         ax[row, col].set_xlim(min(steps), max(steps))
         ax[row, col].set_ylim(0, None)
         if subplot.startswith("clip") and cfg.plot.style.apply_fixed_ylim_clip: