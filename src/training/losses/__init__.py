ed3d4a5ce9fae3c5941b57b2517aa6f1fb8667d4
Morteza Abolghasemi <a.re.morteza@gmail.com>
2026-06-22 16:31:02 +0330

    remove unused file

--- src/training/losses/__init__.py
+++ src/training/losses/__init__.py
@@ -1,10 +1,8 @@
-from .angle_loss import AngleLoss
 from .clip_loss import ClipLoss
 from .contrastive_loss import ContrastiveLoss
 from .losses import CameraTrajectoryLoss
 
 __all__ = [
-    'AngleLoss',
     'ClipLoss',
     'ContrastiveLoss',
     'CameraTrajectoryLoss'
